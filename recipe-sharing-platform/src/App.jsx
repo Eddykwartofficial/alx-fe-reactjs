38fd469cb10e6d31def376c2631c213e9ed422c7
eddykwartofficial <eddykwart4u2@gmail.com>
2025-10-03 08:33:50 +0000

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,43 +1,14 @@
-const App = () => {
-  const [recipes, setRecipes] = useState(initialRecipesData);
-  const [currentPage, setCurrentPage] = useState('home');
-  const [selectedRecipeId, setSelectedRecipeId] = useState(null);
+// src/App.jsx
 
-  const handleRecipeClick = (id) => {
-    setSelectedRecipeId(id);
-    setCurrentPage('detail');
-  };
+import React from 'react';
+import HomePage from './components/HomePage';
 
-  const handleAddRecipe = (newRecipe) => {
-    setRecipes(prev => [newRecipe, ...prev]);
-    setCurrentPage('home');
-  };
-
-  const selectedRecipe = recipes.find(r => r.id === selectedRecipeId);
-
+function App() {
   return (
-    <div>
-      {currentPage === 'home' && (
-        <HomePage
-          recipes={recipes}
-          onRecipeClick={handleRecipeClick}
-          onNavigateToAddRecipe={() => setCurrentPage('add')}
-        />
-      )}
-      {currentPage === 'detail' && (
-        <RecipeDetailPage
-          recipe={selectedRecipe}
-          onBack={() => setCurrentPage('home')}
-        />
-      )}
-      {currentPage === 'add' && (
-        <AddRecipeForm
-          onSubmit={handleAddRecipe}
-          onCancel={() => setCurrentPage('home')}
-        />
-      )}
-    </div>
+    // In later tasks, this component will handle routing between pages.
+    // For now, it just renders the HomePage.
+    <HomePage />
   );
-};
+}
 
 export default App;
